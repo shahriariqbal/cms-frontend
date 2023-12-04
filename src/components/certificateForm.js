8bf81703e75aff13bc01b39ff1aa73eb2c01cfe5
Shahriar Iqbal <shahriar.iqbal.dev@gmail.com>
2023-12-04 16:28:42 +0200

    updated certificateForm component

--- src/components/certificateForm.js
+++ src/components/certificateForm.js
@@ -1,21 +1,57 @@
 import React, { useState } from "react";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Grid } from "@mui/material";
 
 function CertificateForm() {
-  const [certificateData, setCertificateData] = useState({});
+  const [certificateData, setCertificateData] = useState({
+    name: "",
+    description: "",
+  });
 
-  const handleChange = (event) => {};
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCertificateData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(certificateData);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <TextField name="title" onChange={handleChange} required />
-      <Button type="submit" variant="contained">
-        Create Certificate
-      </Button>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            name="name"
+            label="Name"
+            variant="outlined"
+            fullWidth
+            value={certificateData.name}
+            onChange={handleChange}
+            required
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            name="description"
+            label="Description"
+            variant="outlined"
+            fullWidth
+            multiline
+            rows={4}
+            value={certificateData.description}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button type="submit" variant="contained" color="primary">
+            Create Certificate
+          </Button>
+        </Grid>
+      </Grid>
     </form>
   );
 }
